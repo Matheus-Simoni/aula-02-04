256abc1279378e7aae18499550e37a29f2a9e33d
EA - Matheus Henrique Maniá Simoni <matheus_simoni@acad.unasp.edu.br>
2026-04-02 20:13:03 -0300

    Versão final

--- mercado.py
+++ mercado.py
@@ -1,3 +1,4 @@
+import os
 lista = {}
 while True:    
     produto = input('Qual valor voce quer adicionar? ')
@@ -10,8 +11,13 @@ while True:
     continuar = input('Deseja adicionar mais algum produto? ').lower()
 
     if continuar in ['s', 'sim']:
+        os.system('cls')
+        print('\nLista de produtos:')
+        for produto, valor in lista.items():
+            print(f'{produto}: R$ {valor:.2f}')
         continue
     else:
+        os.system('cls')
         print('\nLista de produtos:')
         for produto, valor in lista.items():
             print(f'{produto}: R$ {valor:.2f}')
